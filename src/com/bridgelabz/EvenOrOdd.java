9331c0c3b4a57f4cc3060bfe715887b2df472b2b
Raghu Thilak Gudur Ramadas <thilak.gudur@gmail.com>
2022-11-07 09:01:49 +0530

    creating: creating master branch

---
+++ src/com/bridgelabz/EvenOrOdd.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+public class EvenOrOdd
+{
+
+    public static void main(String[] args) {
+
+        int number=21;
+
+        if(number%2==0)
+        {
+            System.out.println("Given Number:"+number+" is even");
+        }
+        else
+        {
+            System.out.println("Given Number:"+number+" is odd");
+        }
+
+    }
+
+}
---
+++ src/com/bridgelabz/VowelOrConsonent.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+public class VowelOrConsonent {
+
+    public static void main(String[] args)
+    {
+
+        char alphabet='t';
+
+        if(alphabet=='a'||alphabet=='e'||alphabet=='i'||alphabet=='o'||alphabet=='u')
+        {
+            System.out.println("Given Aplbhabet :"+alphabet+" is a vowel");
+        }
+        else
+        {
+            System.out.println("Given Aplbhabet :"+alphabet+" is a Consonant");
+
+        }
+
+    }
+}
